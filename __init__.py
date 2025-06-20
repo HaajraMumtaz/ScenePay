20574c86748402f72f933c19ee06411349348458
HaajraMumtaz <haajra.2102@gmail.com>
2025-06-20 23:33:53 +0500

    added environment variables

---
+++ __init__.py
@@ -0,0 +1,18 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from dotenv import load_dotenv
+
+load_dotenv() 
+db = SQLAlchemy()
+login_manager = LoginManager()
+
+
+def create_app():
+    app = Flask(__name__)
+    
+    app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+
+    return app
--- init_db.py
+++ init_db.py
@@ -1,5 +1,10 @@
-from SUMMER_PROJECT import app, db
-from models import *
+import sys
+print("-----")
+print(sys.path)
+print("------2")
+from billSplitter import create_app, db
+from billSplitter.models import *
 
 with app.app_context():
     db.create_all()
+    print("✅ Database tables created.")
