efaafbdc9b08505424feea4a1cb72ebdb4e722d2
HaajraMumtaz <haajra.2102@gmail.com>
2025-07-14 23:00:28 +0500

    added receipt upload form

--- routes/upload_routes.py
+++ routes/upload_routes.py
@@ -1,13 +1,14 @@
 from flask import Blueprint, render_template, request, redirect, url_for, session
+from ..utils.parse import parse_bill_text
 upload_bp = Blueprint('upload', __name__)
 
-@upload_bp.route("/upload", methods=["GET", "POST"])
+@upload_bp.route('/group/<int:group_id>/upload_receipt', methods=["GET", "POST"])
 def upload_receipt():
     if request.method == "POST":
         file = request.files.get("receipt")
         if file:
-            parsed_data = extract_data_from_image(file)  # returns dict of {item: price}
+            parsed_data = parse_bill_text(file) 
             session['ocr_items'] = parsed_data
-        return redirect(url_for('form.form'))  # Move to next form
+        return redirect(url_for('form.form')) 
 
     return render_template("upload.html")
